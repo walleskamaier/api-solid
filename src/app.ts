006c3f3060f90168b548e98a2ebaa0e6fcc1bd0f
Walleska Maier <walleskamaier@gmail.com>
2025-10-06 11:03:35 -0300

    chore: add ESLint and Prisma configuration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify"
+import { PrismaClient } from "@prisma/client"
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: "Walleska Maier",
+    email: "walleskamaier@gmail.com",
+  },
+})
