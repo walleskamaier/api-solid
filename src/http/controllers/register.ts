033af1a5b71792550a8f59b1f6c3b90051e87331
Walleska Maier <walleskamaier@gmail.com>
2025-10-15 13:47:32 -0300

    refactor(auth): extract user registration logic into register use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import type { FastifyRequest, FastifyReply } from "fastify"
 import { z } from "zod"
-import { prisma } from "@/lib/prisma.js"
-import { hash } from "bcryptjs"
+import { registerUseCase } from "@/use-cases/register.js"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,27 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send({
-      message: "E-mail already registered.",
-    })
+  try {
+    await registerUseCase({ name, email, password })
+  } catch {
+    return reply.status(409).send()
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
 
   return reply.status(201).send()
 }
