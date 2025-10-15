acd27aaca2560e7e2ed501fc8c1dadd6c6625d9d
Walleska Maier <walleskamaier@gmail.com>
2025-10-15 13:14:38 -0300

    feat(auth): hash password and validate unique email during registration

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,9 @@
 import type { FastifyRequest, FastifyReply } from "fastify"
 import { z } from "zod"
 import { prisma } from "@/lib/prisma.js"
+import { hash } from "bcryptjs"
 
-export async function register (request: FastifyRequest , reply: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.email(),
@@ -11,11 +12,25 @@ export async function register (request: FastifyRequest , reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: "E-mail already registered.",
+    })
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
