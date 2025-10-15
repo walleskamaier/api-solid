a8a54f77c41b2c2a188b8d0c63d3c2b852fe448a
Walleska Maier <walleskamaier@gmail.com>
2025-10-15 15:40:28 -0300

    refactor: create PrismaUsersRepository and use it in RegisterUseCase

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma.js"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository.js"
 import { hash } from "bcryptjs"
 
 interface RegisterUseCaseRequest {
@@ -7,24 +8,28 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({ name, email, password }: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6)
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
 
-    if (userWithSameEmail) {
-      throw new Error("E-mail already registered.")
-    }
+  if (userWithSameEmail) {
+    throw new Error("E-mail already registered.")
+  }
 
-    await prisma.user.create({
-      data: {
-        name,
-        email,
-        password_hash,
-      },
-    })
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
 }
