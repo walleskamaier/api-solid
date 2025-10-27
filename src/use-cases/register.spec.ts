10f35d4da2f80f63cfba8b7194bbbc5e92d6f164
Walleska Maier <walleskamaier@gmail.com>
2025-10-27 10:19:30 -0300

    test(register): verify password hashing behavior

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest"
 import { RegisterUseCase } from "./register.js"
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository.js"
+import { compare } from "bcryptjs"
 
 describe("Register use case", () => {
   it("should be able to register", async () => {
@@ -14,5 +15,23 @@ describe("Register use case", () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
