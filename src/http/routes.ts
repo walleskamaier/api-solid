80e568f8c534904f2c98c77ab14d351dbea4401b
Walleska Maier <walleskamaier@gmail.com>
2025-10-28 17:43:22 -0300

    feat(http): add sessions route for authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify"
 import { register } from "./controllers/register.js"
+import { authenticate } from "./controllers/authenticate.js"
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register)
+  app.post("/sessions", authenticate)
 }
