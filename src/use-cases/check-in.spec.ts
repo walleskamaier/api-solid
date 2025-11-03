1e6a49966a6f7040c9a56e2fef7cd19a4bf80e2a
Walleska Maier <walleskamaier@gmail.com>
2025-11-03 15:39:06 -0300

    test(check-ins): add CheckIn use case test

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,23 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import type { CheckInsRepository } from "@/repositories/check-ins-repository.js"
+import { CheckInUseCase } from "./check-in.js"
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository.js"
+
+let checkInsRepository: CheckInsRepository
+let sut: CheckInUseCase
+
+describe("Check-in use case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it("should be able to check in", async () => {
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
