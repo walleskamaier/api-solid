cb33f0ce55876ee0ed612b8e2ee931cfd4102b45
Walleska Maier <walleskamaier@gmail.com>
2025-11-10 14:06:37 -0300

    feat(gyms): add search gyms use case

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,26 @@
+import type { GymsRepository } from "@/repositories/gyms-repository.js"
+import type { Gym } from "@prisma/client"
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
