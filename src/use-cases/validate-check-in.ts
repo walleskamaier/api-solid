92d8ea6042e7ffd6321c78361cf17535a55bafb4
Walleska Maier <walleskamaier@gmail.com>
2025-11-11 14:27:37 -0300

    feat(check-ins): add validate check-in use case

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,33 @@
+import type { CheckIn } from "@prisma/client"
+import type { CheckInsRepository } from "@/repositories/check-ins-repository.js"
+import { ResourceNotFoundError } from "./errors/resource-not-found-error.js"
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
